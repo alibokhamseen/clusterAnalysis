05ad04300799a29068175dac4b0fad87f085ddb8
MitchelUTexas <114682124+MitchelUTexas@users.noreply.github.com>
2022-10-04 22:59:59 -0500

    Improved coordinate matching between gaia and simbad

--- pipeline.py
+++ pipeline.py
@@ -1,18 +1,16 @@
 #Finds Distance estimates for a specified cluster
 #Error propigation via taylor approximation (assuming symettric gaussian errors)
-    #TODO: Use cubic formula for photometric system conversions
     #TODO: Change to finer Spectral class bins
-
-    #TODO: get average cluster metallicity (gspspec)
     #TODO: get virial mass
-    #TODO: add extra data analysis plots (i.e., 2D coords, color-color)
     #TODO: add cepheid PL(C) distance estimate
+
+    #TODO: get better metallicity source
     #TODO: add pipeline for queries (gaia, simbad)
+    #TODO: Use cubic formula for photometric system conversions (or otherwise improve the algorithm)
 
     #TODO: add ZAMS MS fit
     #TODO: add isochrone fit, also get age
     #TODO: add better cluster membership filtering with algorithms
-    #TODO: optimize everything
 
 
 #Imports
@@ -24,6 +22,7 @@ import pandas as pd
 from scipy.optimize import curve_fit
 from scipy import stats as stats_sci
 import statistics as stat
+import sys
 from uncertainties import ufloat
 from uncertainties.umath import *
 
@@ -34,7 +33,7 @@ pd.set_option("mode.chained_assignment", None)
 def main():
 #----------SET USER PARAMETERS HERE---------
     #Set z-score for pm exclusion
-    z=2
+    z=1
     #Choose GAIA CSV input file name
     GAIAname = "Hyades-result.csv"
     SIMBADname = "simbad.txt"
@@ -45,22 +44,22 @@ def main():
     gaia = pd.read_csv(GAIAname)
     simbad = pd.read_csv(SIMBADname,sep="|")
     simbad = simbad.rename(columns=lambda x: x.strip())
+    simbad = simbad.rename(columns=lambda x: "spec_type" if x=="spec. type" else x)
 
     #Parse Simbad data
-    simbad["spec. type"] = simbad["spec. type"].str.strip()
+    simbad["spec_type"] = simbad["spec_type"].str.strip()
         #Keep only main sequence stars (luminosity class 5) with spectral types
-    simbad = simbad[simbad["spec. type"].str.match("^[OBAFGKM]\d+(\.\d*)?V$",na=False)]
+    simbad = simbad[simbad["spec_type"].str.match("^[OBAFGKM]\d+(\.\d*)?V$",na=False)]
         #remove duplicates by Ra, Dec
     simbad.drop_duplicates(subset=["coord1 (ICRS,J2000/2000)"])
-    print("Num Simbad stars = ", simbad.shape[0])
         #Ignoring 'A value is trying to be set on a copy of a slice from a DataFrame.' warnings, this is what I am trying to do
         #Conver Ra,Dec to decimal degrees
-    simbad["Ra"] = simbad["coord1 (ICRS,J2000/2000)"].map(lambda x: float(x.split()[0])*(15) + float(x.split()[1])*(15/60) + float(x.split()[2])*(15/3600))
+    simbad["ra"] = simbad["coord1 (ICRS,J2000/2000)"].map(lambda x: float(x.split()[0])*(15) + float(x.split()[1])*(15/60) + float(x.split()[2])*(15/3600))
     simbad["Dec"] = simbad["coord1 (ICRS,J2000/2000)"].map(lambda x: float(x.split()[3]) + float(x.split()[4])*(1/60) + float(x.split()[5])*(1/3600))
         #Drop old coordinate format
     simbad = simbad.drop(columns=["coord1 (ICRS,J2000/2000)"])
 
-    #Filtering data for nonvalues and similar apparant magnitudes
+    #Filtering gaia data for nonvalues and similar apparant magnitudes
     maxMag = 10
     gaia = gaia.dropna(subset=["phot_g_mean_mag","phot_bp_mean_mag","phot_rp_mean_mag","parallax"])
     gaia = gaia[(gaia["phot_g_mean_mag"] < maxMag) & (gaia["phot_bp_mean_mag"] < maxMag) & (gaia["phot_rp_mean_mag"] < maxMag) & (gaia["parallax"] > 0)]
@@ -68,6 +67,16 @@ def main():
     #Get trig parallax distance to each star
     gaia["dist_trig_parallax"] = 1/(gaia["parallax"]/1000)
     gaia["err_dist_trig_parallax"] = np.abs(-1000/(gaia["parallax"]**2)*gaia["parallax_error"])
+
+    #print different databases' ra, dec
+    plt.figure()
+    plt.title("Ra, Dec of Database Stars")
+    plt.scatter(gaia["ra"],gaia["dec"],label="gaia",color='blue')
+    plt.scatter(simbad["ra"],simbad["Dec"],label='simbad',color='red')
+    plt.xlabel("Ra [degrees]")
+    plt.ylabel("Dec [degrees]")
+    plt.legend()
+    plt.show()
 
     #Join gaia and simbad data
     data = coordMatch(gaia,simbad)
@@ -92,19 +101,46 @@ def main():
 
     #Get cluster members by removing pm and parallax z-std outliers
     data = getMems(data,z)
+    print("Num Members =", data.shape[0])
 
     #Get intrinsic magnitudes and color excesses from spec types
     data = getSpecParams(data)
+
+    #get cluster metallicity
+    FeH = np.nanmean( data["fem_gspspec"] - data["mh_gspspec"] )
+    FeH_lower = np.nanmean( data["fem_gspspec_lower"] - data["mh_gspspec_lower"] )
+    FeH_upper = np.nanmean( data["fem_gspspec_upper"] - data["mh_gspspec_upper"] )
+        #drop intermediary values
+    data = data.drop(columns=["fem_gspspec","fem_gspspec_lower","fem_gspspec_upper","mh_gspspec","mh_gspspec_lower","mh_gspspec_upper"])
+    print("Fe/H =", figRound(FeH), ", lowerConf =", figRound(FeH_lower), ", upperConf = ", figRound(FeH_upper))
 
     #gaia g,bp,rp --> Johnson U,B,V
         #Conversions from https://gea.esac.esa.int/archive/documentation/GDR2/Data_processing/chap_cu5pho/sec_cu5pho_calibr/ssec_cu5pho_PhotTransf.html#Ch5.T8
     data["V_app"] = data["phot_g_mean_mag"] + .01760 + .006860*(data["phot_bp_mean_mag"] - data["phot_rp_mean_mag"]) + .1732*(data["phot_bp_mean_mag"] - data["phot_rp_mean_mag"])**2
     data["bp-rp"] = data["phot_bp_mean_mag"] - data["phot_rp_mean_mag"]
-        #solving a cubic polynomial for (B-V)_app, picking largest root
+        #solving a cubic polynomial for apparent colors, picking largest root (formulas from setting f(G_bp-G_rp)=f(color)=G-V)
     data["(B-V)_app"] = data["bp-rp"].map(lambda x: np.max(np.real(np.poly1d([-.001768,-.2297,-.02385,-.01147+.006860*(x)+.1732*(x**2)]).roots)) )
-        #set err_V_app = .05 mag, err_(B-V)_app=.08 (approx. per conversion formulas)
+    data["(V-R)_app"] = data["bp-rp"].map(lambda x: np.max(np.real(np.poly1d([0.2225,-1.016,-0.01784,-.00509+.006860*(x)+.1732*(x**2)]).roots)) )
+        #set errors (approx. per conversion formulas)
     data["err_V_app"] = .05
     data["err_(B-V)_app"] = .08
+    data["err_(V-R)_app"] = .07
+    
+    #Plot color-color
+        #gaia colors
+    plt.figure()
+    plt.title("Color-Color (not dereddened)")
+    plt.scatter(data["phot_g_mean_mag"] - data["phot_bp_mean_mag"],data["phot_bp_mean_mag"] - data["phot_rp_mean_mag"])
+    plt.xlabel("(G-BP)_app [mag]")
+    plt.ylabel("(BP-RP)_app [mag]")
+    plt.show()
+        #johnson colors
+    plt.figure()
+    plt.title("Color-Color (not dereddened)")
+    plt.scatter(data["(B-V)_app"],data["(V-R)_app"])
+    plt.xlabel("(B-V)_app [mag]")
+    plt.ylabel("(V-R)_app [mag]")
+    plt.show()
         #drop gaia magnitudes
     data = data.drop(columns=["phot_g_mean_mag","phot_bp_mean_mag","phot_rp_mean_mag","bp-rp"])
 
@@ -122,10 +158,18 @@ def main():
     plt.hist((data["dist_trig_parallax"]), bins=100, range=(0,1000))
     plt.show()
 
+    #Plot cluster ra, dec
+    plt.figure()
+    plt.title("Ra, Dec of Cluster Stars")
+    plt.scatter(data["ra"],data["dec"])
+    plt.xlabel("Ra [degrees]")
+    plt.ylabel("Dec [degrees]")
+    plt.show()
+
     #Plot Observed CMD
     plt.figure()
     plt.gca().invert_yaxis()
-    plt.title("Observed CMD of Hyades Cluster")
+    plt.title("Observed CMD")
     plt.xlabel("(B-V)_app [Mag]")
     plt.ylabel("V_app [Mag]")
     plt.scatter(data["(B-V)_app"],data["V_app"], color="red", label = "Hyades Stars")
@@ -139,12 +183,13 @@ def main():
     #Plot corrected CMD
     plt.figure()
     plt.gca().invert_yaxis()
-    plt.title("Corrected CMD of Hyades Cluster")
+    plt.title("Corrected CMD")
     plt.xlabel("(B-V)_intr [Mag]")
     plt.ylabel("V_rel [Mag]")
     plt.scatter(data["(B-V)_intr"],data["V_rel"], color="red", label = "Hyades Stars")
     plt.legend()
     plt.show()
+
 
     #Get Distance Estimates and print them
         #Trig parallax
@@ -168,8 +213,8 @@ def getMems(stars,z):
 #Applies Variable Extinction Method and returns R value and distance estimate
 def varExt(stars):
         #Regresson to get R
-        #errors estimated by taking sigma=sqrt(err_dMod_app**2 + (3.1)**2 * err_colorExcess)
-    popt,pcov = curve_fit(ext_fit, stars["colorExcess"], stars["dMod_app"],sigma=np.sqrt(stars["err_dMod_app"]**2 + (3.1)**2 * stars["err_colorExcess"]))
+        #errors estimated by taking sigma=sqrt(err_dMod_app**2 + (3.1)**2 * err_colorExcess**2)
+    popt,pcov = curve_fit(ext_fit, stars["colorExcess"], stars["dMod_app"],sigma=np.sqrt(stars["err_dMod_app"]**2 + (3.1)**2 * stars["err_colorExcess"]**2))
         #get fit parameters
     perr = np.sqrt(np.diag(pcov))
     r = popt[0]
@@ -234,40 +279,22 @@ def sciRound(num,err):
 
 #match gaia and simbad data by RA, Dec
 def coordMatch(gaia, simbad):
-        #maximum number of decmals to round to is 14
-    rNum = 14
-    bestRatio = 0
-        #loop to find number of decimals to round, pick iteration that made most matches (excluding overmatching, i.e. more matches than origional stars)
-        #if multiple stars appear to match, pick one with least angular seperation
-    while rNum > 0:
-        gaia["Rra"] = gaia["ra"].round(rNum)
-        gaia["Rdec"] = gaia["dec"].round(rNum)
-        simbad["Rra"] = simbad["Ra"].round(rNum)
-        simbad["Rdec"] = simbad["Dec"].round(rNum)
-        matches = gaia.merge(simbad,how="inner",on=["Rra","Rdec"])
-        matches["angSep"] = (matches["ra"]-matches["Ra"])**2 + (matches["dec"]-matches["Dec"])**2
-        matches.sort_values("angSep")
-        matches = matches.drop_duplicates(subset=["Rra","Rdec"])
-        matchRatio = matches.shape[0] / gaia.shape[0]
-        if (matchRatio > bestRatio) and (matchRatio <= 1):
-            bestNum = rNum
-            bestRatio = matchRatio
-        rNum -= 1
-    if bestRatio == 0:
-        print("ERROR: Cannot Match Gaia to simbad data")
-        return "Error"
-    else:
-        gaia["Rra"] = gaia["ra"].round(bestNum)
-        gaia["Rdec"] = gaia["dec"].round(bestNum)
-        simbad["Rra"] = simbad["Ra"].round(bestNum)
-        simbad["Rdec"] = simbad["Dec"].round(bestNum)
-        matches = gaia.merge(simbad,how="inner",on=["Rra","Rdec"])
-        matches["angSep"] = (matches["ra"]-matches["Ra"])**2 + (matches["dec"]-matches["Dec"])**2
-        matches.sort_values("angSep")
-        matches = matches.drop_duplicates(subset=["Rra","Rdec"])
-        matches.drop(columns=["Rra","Rdec","angSep"])
-        print("Successfully matched " + str(matches.shape[0]) + " / " + str(gaia.shape[0]) + " objects in the gaia to simbad data")
-        return matches
+        #merge_asof needs sorted dataframes
+    gaia = gaia.sort_values(by="ra")
+    simbad = simbad.sort_values(by="ra")
+        #get join tolerance from average ra,dec error in gaia
+        #sqrt 2 factor approximates additon of error from simbad
+    tolRa = np.abs( np.nanmean(gaia["ra_error"])) / 2 * np.sqrt(2)
+    tolDec = np.abs( np.nanmean(gaia["dec_error"])) / 2 * np.sqrt(2)
+        #merge_asof only allows merge on one columns, so drop collumns that are above error for dec
+    matches = pd.merge_asof(gaia,simbad,on='ra',tolerance=tolRa,direction='nearest')
+    matches = matches[np.abs(matches["dec"] - matches["Dec"]) <= tolDec]
+        #renaming merged columns
+    matches = matches.rename(columns={'ra_x':'ra'})
+    if matches.shape[0] == 0:
+        print("ERROR: No Coordinate Matches")
+        sys.exit()
+    return matches
 
 #Get intrinsic color and magnitude from spectral type
     #Use Tsvetkov's tabulated values (https://link.springer.com/content/pdf/10.1134/S1063773708010039.pdf)
@@ -278,19 +305,19 @@ def getSpecParams(frame):
     order = "OBAFGKM_"
     order2 = ["O5","O5","O9","B0","B5","A0","A5","F0","F5","G0","G5","K0","K5","M0","M5","M5"]
         #deal with different O-type numbers in tabulated data
-    frame["spec. type"] = frame["spec. type"].map(lambda x: "O" + str(float(x[1:-1])+5) + "V" if x[:1]=="O" else x)
+    frame["spec_type"] = frame["spec_type"].map(lambda x: "O" + str(float(x[1:-1])+5) + "V" if x[:1]=="O" else x)
         #spec types round to nearest tabulated type
-    frame["spec. type"] = frame["spec. type"].map(lambda x: x[:1] + str(5*round(float(x[1:-1])/5)) if (5*round(float(x[1:-1])/5) < 10) else order[order.index(x[:1])+1] + "0")
+    frame["spec_type"] = frame["spec_type"].map(lambda x: x[:1] + str(5*round(float(x[1:-1])/5)) if (5*round(float(x[1:-1])/5) < 10) else order[order.index(x[:1])+1] + "0")
         #deal with M5 being last listed type
-    frame["spec. type"] = frame["spec. type"].map(lambda x: "M5" if (x=="_0") else x)
+    frame["spec_type"] = frame["spec_type"].map(lambda x: "M5" if (x=="_0") else x)
         #get intrinsic/tabulated values
         #estimate error as average half-difference to adjacent bins
-    frame["V_intr"] = frame["spec. type"].map(lambda x: tabV[x])
-    frame["err_V_intr"] = frame["spec. type"].map(lambda x: np.abs(tabV[order2[order2.index(x)-1]]) + np.abs(tabV[order2[order2.index(x)+1]])/2 if (order2.index(x) != 0) else np.abs(tabV[order2[order2.index(x)+1]])/2)
-    frame["(B-V)_intr"] = frame["spec. type"].map(lambda x: tabB_V[x])
-    frame["err_(B-V)_intr"] = frame["spec. type"].map(lambda x: np.abs(tabB_V[order2[order2.index(x)-1]]) + np.abs(tabB_V[order2[order2.index(x)+1]])/2 if (order2.index(x) != 0) else np.abs(tabB_V[order2[order2.index(x)+1]])/2)
+    frame["V_intr"] = frame["spec_type"].map(lambda x: tabV[x])
+    frame["err_V_intr"] = frame["spec_type"].map(lambda x: np.abs(tabV[order2[order2.index(x)-1]]) + np.abs(tabV[order2[order2.index(x)+1]])/2 if (order2.index(x) != 0) else np.abs(tabV[order2[order2.index(x)+1]])/2)
+    frame["(B-V)_intr"] = frame["spec_type"].map(lambda x: tabB_V[x])
+    frame["err_(B-V)_intr"] = frame["spec_type"].map(lambda x: np.abs(tabB_V[order2[order2.index(x)-1]]) + np.abs(tabB_V[order2[order2.index(x)+1]])/2 if (order2.index(x) != 0) else np.abs(tabB_V[order2[order2.index(x)+1]])/2)
         #drop spec types
-    frame = frame.drop(columns=["spec. type"])
+    frame = frame.drop(columns=["spec_type"])
     return frame
 
 #Run Main

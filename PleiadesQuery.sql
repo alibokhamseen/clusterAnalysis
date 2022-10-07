f979d51b74c82493852f15a8c7ec72637273e000
MitchelUTexas <114682124+MitchelUTexas@users.noreply.github.com>
2022-10-06 21:52:59 -0500

    Sample Queries

---
+++ PleiadesQuery.sql
@@ -0,0 +1,15 @@
+select
+    gs.source_id, gs.ra, gs.ra_error, gs.dec, gs.dec_error, gs.parallax, gs.parallax_error, gs.pmra, gs.pmra_error, gs.pmdec, gs.pmdec_error,
+    gs.radial_velocity, gs.radial_velocity_error,
+    gs.phot_g_mean_mag, gs.phot_bp_mean_mag, gs.phot_rp_mean_mag,
+    ap.mh_gspspec, ap.mh_gspspec_lower, ap.mh_gspspec_upper, ap.fem_gspspec, ap.fem_gspspec_lower, ap.fem_gspspec_upper
+from
+    gaiadr3.gaia_source as gs
+join gaiadr3.astrophysical_parameters as ap using (source_id)
+where
+    (gs.phot_g_mean_mag is not Null)
+    and (gs.phot_bp_mean_mag is not Null)
+    and (gs.phot_rp_mean_mag is not Null)
+    and (1 = CONTAINS(
+        POINT(56.85, 24.1167),
+        CIRCLE(gs.ra, gs.dec, 300./60.)))
